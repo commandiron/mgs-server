dd3518468edb20a1093b15a468358a521682287c
commandiron <commandiron@gmail.com>
2023-01-09 17:07:09 +0300

    .

--- src/main/kotlin/com/commandiron/routes/characters/SearchCharacters.kt
+++ src/main/kotlin/com/commandiron/routes/characters/SearchCharacters.kt
@@ -1,5 +1,6 @@
 package com.commandiron.routes.characters
 
+import com.commandiron.models.character.CharacterApiResponse
 import com.commandiron.repository.characters.CharactersRepository
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -11,16 +12,23 @@ fun Route.searchCharacters() {
     val charactersRepository: CharactersRepository by inject()
 
     get("/mgs/characters/search") {
-        val name = call.request.queryParameters["name"]
+        try {
+            val name = call.request.queryParameters["name"]
 
-        val page = call.request.queryParameters["page"]?.toInt() ?: 1
-        val limit = call.request.queryParameters["limit"]?.toInt() ?: 5
+            val page = call.request.queryParameters["page"]?.toInt() ?: 1
+            val limit = call.request.queryParameters["limit"]?.toInt() ?: 5
 
-        val apiResponse = charactersRepository.searchCharacters(name, page, limit)
+            val apiResponse = charactersRepository.searchCharacters(name, page, limit)
 
-        call.respond(
-            message = apiResponse,
-            status = HttpStatusCode.OK
-        )
+            call.respond(
+                message = apiResponse,
+                status = HttpStatusCode.OK
+            )
+        } catch (e: IllegalArgumentException) {
+            call.respond(
+                message = CharacterApiResponse(success = false, message = "Characters Not Found."),
+                status = HttpStatusCode.NotFound
+            )
+        }
     }
 }
